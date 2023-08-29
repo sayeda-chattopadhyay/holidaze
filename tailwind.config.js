898cacaf9d79235fc88cc59ed14bf1c0197b5e4a
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-08-29 17:17:24 +0200

    adds custom color, font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0F3CF0",
+        secondary: {
+          100: "#F0F0F0",
+          200: "#E0E0E0",
+        },
+        fontFamily: {
+          roboto: ["Roboto"],
+        },
+        backgroundImage: {
+          "hero-pattern": "url('/src/assets/images/hero-image.png')",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
