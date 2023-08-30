6aa9647a0ecb37f6fdad7b8096e011f700f921a0
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-08-30 13:08:43 +0200

    adds static content to home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,47 @@
+import Banner from "../../components/banner/Banner";
+import Card from "../../components/cards/Card";
+import SearchBar from "../../components/search/SearchBar";
+
 const HomePage = () => {
   return (
     <>
-      <h1>Home page</h1>
+      <div>
+        <Banner />
+        <h1>Find Your best destination</h1>
+        <SearchBar />
+        <Card />
+        <section>
+          <h2>Why must choose us</h2>
+          <div>
+            <div>icon</div>
+            <div>
+              <h3>Lot of Choices</h3>
+              <p>Many choices of attractive vacation</p>
+            </div>
+          </div>
+        </section>
+        <section className="px-6 border-4 border-red-300">
+          <div className="flex flex-col items-center md:flex-row justify-center md:space-x-12 px-10 gap-10">
+            <div>
+              <h3>Ready to transform spaces into memorable experiences?</h3>
+              <p className="font-paragraph text-gray-500 max-w-md">
+                Unlock a world of opportunities by becoming a Venue Manager! As
+                a host, you will have the power to create unforgettable
+                experiences and bring your unique spaces to life. Join our
+                platform and take the first step towards showcasing your venue
+                to a diverse and eager audience. Register now and let the
+                journey of hosting begin!
+              </p>
+              <button>Register as a host</button>
+            </div>
+            <img
+              src="/src/assets/images/become-host.jpg"
+              alt="picture is showing a person is standing infront of check-in reception desk."
+              className="h-48 w-full object-cover md:h-full md:w-48"
+            />
+          </div>
+        </section>
+      </div>
     </>
   );
 };
