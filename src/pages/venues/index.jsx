c5b691beeeb4b2afb1cfa962a9fb1bd59d3a84cd
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-09-01 13:30:17 +0200

    organizes venue page

--- src/pages/venues/index.jsx
+++ src/pages/venues/index.jsx
@@ -1,34 +1,11 @@
-// import { useState } from "react"
-import ApiHook from "../../hooks/ApiHook";
-import { VENUES_URL } from "../../constants";
-import Card from "../../components/cards/Card";
+import AllVenues from "./AllVenues";
 
-const Venues = () => {
-  const { data, isLoading, isError } = ApiHook(VENUES_URL);
-
-  if (isLoading) {
-    return <div>Loading</div>;
-  }
-
-  if (isError) {
-    return <div>Error</div>;
-  }
-
-  const venues = data;
-
-  console.log(venues);
-
+const VenuesPage = () => {
   return (
-    <>
-      <div className="search"> Search</div>
-
-      <div className="grid grid-cols-1 md:grid-cols-3 place-items-center mt-10 border-4 border-green-800 px-10 py-10 ">
-        {venues.map((venue, index) => (
-          <Card key={index} venue={venue} />
-        ))}
-      </div>
-    </>
+    <div>
+      <AllVenues />
+    </div>
   );
 };
 
-export default Venues;
+export default VenuesPage;
