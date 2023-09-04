a19e72bf7cc4d08e0835638a564e3a2897641f69
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-09-04 13:46:29 +0200

    adds url

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,2 +1,4 @@
 export const BASE_URL = "https://nf-api.onrender.com/api/v1/holidaze/";
 export const VENUES_URL = `${BASE_URL}venues`;
+export const REGISTER_URL = `${BASE_URL}auth/register`;
+export const LOGIN_URL = `${BASE_URL}auth/login`;
