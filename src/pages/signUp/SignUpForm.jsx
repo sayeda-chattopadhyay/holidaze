b117186ead5fbc57acbfedb20a175932eaeae18c
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-09-04 13:45:34 +0200

    creates signup form using formik & yup

---
+++ src/pages/signUp/SignUpForm.jsx
@@ -0,0 +1,247 @@
+import { NavLink } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+// RegEx values
+
+const emailNoroffRegex = /^[A-Z0-9._%+-]+@stud.noroff\.no$/i;
+
+const validationSchema = Yup.object({
+  name: Yup.string()
+    .min(3, "your name should be at least 3 character")
+    .required("Username is required"),
+  email: Yup.string()
+    .email("Invalid email format")
+    .matches(emailNoroffRegex, "the email must be your noroff email")
+    .required("Please enter your noroff email"),
+  password: Yup.string().required("Required"),
+  avatar: Yup.string().url("Invalid url"),
+  venueManager: Yup.boolean(),
+});
+
+const SignUpForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+      avatar: "",
+      venueManager: false,
+    },
+
+    onSubmit: (values, action) => {
+      const formData = {
+        name: values.name,
+        email: values.email,
+        password: values.password,
+        avatar: values.avatar,
+        venueManager: values.venueManager,
+      };
+
+      action.resetForm();
+      console.log("Form data:", formData);
+
+      //  send to api
+    },
+
+    validationSchema,
+  });
+  console.log("Formik error : ", formik.errors);
+  console.log("Formik touched: ", formik.touched);
+
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign Up
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={formik.handleSubmit}>
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Name
+              </label>
+              <div className="mt-2">
+                <input
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  id="name"
+                  name="name"
+                  type="text"
+                  autoComplete="off"
+                  required
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.name}
+                />
+                {formik.touched.name && formik.errors.name ? (
+                  <div className="text-red-500 text-sm">
+                    {formik.errors.name}
+                  </div>
+                ) : null}
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email
+              </label>
+              <div className="mt-2">
+                <input
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="off"
+                  required
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                />
+                {formik.touched.email && formik.errors.email ? (
+                  <div className="text-red-500 text-sm">
+                    {formik.errors.email}
+                  </div>
+                ) : null}
+              </div>
+            </div>
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="off"
+                  required
+                  onChange={formik.handleChange}
+                  value={formik.values.password}
+                  onBlur={formik.handleBlur}
+                />
+                {formik.touched.password && formik.errors.password ? (
+                  <div className="text-red-500 text-sm">
+                    {formik.errors.password}
+                  </div>
+                ) : null}
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="avatar"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Avatar
+              </label>
+              <div className="mt-2">
+                <input
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  id="avatar"
+                  name="avatar"
+                  type="avatar"
+                  autoComplete="off"
+                  required
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.avatar}
+                />
+                {formik.touched.avatar && formik.errors.avatar ? (
+                  <div className="text-red-500 text-sm">
+                    {formik.errors.avatar}
+                  </div>
+                ) : null}
+              </div>
+            </div>
+
+            <p>How do you like to register ?</p>
+            {/* radio button start */}
+            {/* <div className="flex justify-between items-center gap-2">
+              <input
+                type="radio"
+                id="no"
+                name="venueManager"
+                value={false}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                checked={formik.values.venueManager === false}
+              />
+              <label htmlFor="no">Guest</label>
+            </div>
+            <div className="flex justify-between items-center">
+              <input
+                type="radio"
+                id="yes"
+                name="venueManager"
+                value={true}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                checked={formik.values.venueManager === true}
+              />
+              <label htmlFor="yes">Venue Manager</label>
+            </div> */}
+            {/* radio button ends */}
+            <div>
+              <label
+                htmlFor="venueManager"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Select Role
+              </label>
+              <div className="mt-2">
+                <select
+                  id="venueManager"
+                  name="venueManager"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.venueManager}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                >
+                  <option value={false}>Guest</option>
+                  <option value={true}>Venue Manger</option>
+                </select>
+              </div>
+            </div>
+            {/* submit button */}
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign Up
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Already have an account ?{" "}
+            <NavLink to="/login" className="font-bold text-blue-700">
+              {" "}
+              Log In
+            </NavLink>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignUpForm;
