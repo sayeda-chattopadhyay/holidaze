61d1438eb8094b230bdb85d720afd22b1c3beb4f
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-09-08 00:26:24 +0200

    api hook

--- src/hooks/ApiHook.jsx
+++ src/hooks/ApiHook.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 /**
  * Custom hook to fetch data from an API endpoint.
@@ -7,29 +7,29 @@ import { useState, useEffect } from 'react';
  */
 
 const ApiHook = (url) => {
-    const [data, setData] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [isError, setIsError] = useState(false); // Fetch data from API endpoint
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false); // Fetch data from API endpoint
 
-useEffect(() => {
+  useEffect(() => {
     async function fetchData() {
-        try {
-            setIsError(false);
-            setIsLoading(true);
-            const response = await fetch(url);
-            const data = await response.json();
-            setData(data);
-        } catch (error) {
-            console.log(error);
-            setIsError(true);
-        }finally{
-            setIsLoading(false);
-        }
+      try {
+        setIsError(false);
+        setIsLoading(true);
+        const response = await fetch(url);
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        console.log(error);
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
     }
     fetchData();
-}, [url])
+  }, [url]);
 
-  return { data, isLoading, isError}
-}
+  return { data, isLoading, isError };
+};
 
-export default ApiHook
+export default ApiHook;
