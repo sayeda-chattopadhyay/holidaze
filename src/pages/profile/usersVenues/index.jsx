21e9a1f9fd746d7725d5489f211705d91da7e921
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-09-15 22:45:59 +0200

    creates uservenue card

--- src/pages/profile/usersVenues/index.jsx
+++ src/pages/profile/usersVenues/index.jsx
@@ -1,11 +1,9 @@
-const BookingVenueCard = ({ booking }) => {
-  const {
-    guests,
-    dateFrom,
-    dateTo,
-    id,
-    venue: { name, location, meta, media, price, rating },
-  } = booking;
+const UserVenueCard = ({ userVenue }) => {
+    console.log("userVenue", userVenue);
+    const {id, name, description, created,location, maxGuests,media} = userVenue;
+
+    console.log("name", name);
+//   const {  media, created, maxGuests } = userVenue;
 
   function formatDate(dateString) {
     const date = new Date(dateString);
@@ -16,23 +14,20 @@ const BookingVenueCard = ({ booking }) => {
   }
 
   return (
-    <div className="container px-4 py-4 ">
+    <div className="container px-4 py-4  ">
       <div className="max-w-xl mx-auto bg-white px-2 py-2 rounded-lg overflow-hidden shadow-md md:flex md:flex-row md:gap-10 justify-between items-center">
         <img
           className="w-full h-48 object-cover md:w-1/2 rounded-lg shadow-sm"
-          src={media}
+          src={media[0]}
           alt={name}
         />
         <div className="p-4 md:w-1/2">
           <h2 className="text-2xl font-semibold mb-2">{name}</h2>
-          <p className="text-sm text-gray-600 mb-2">{location.city} , {location.country}</p>
           <p className="text-sm text-gray-600 mb-2">
-            Check-in: {formatDate(dateFrom)}
+            Created: {formatDate(created)}
           </p>
-          <p className="text-sm text-gray-600 mb-2">
-            Check-out: {formatDate(dateTo)}
-          </p>
-          <p className="text-sm text-gray-600 mb-2">Guests: {guests}</p>
+
+          <p className="text-sm text-gray-600 mb-2">Max Guests: {maxGuests}</p>
           <div className="flex justify-between mt-4">
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
               View More
@@ -40,10 +35,11 @@ const BookingVenueCard = ({ booking }) => {
           </div>
         </div>
       </div>
+      <div>
+        <h1>Create New Venue</h1>
+      </div>
     </div>
   );
 };
 
-export default BookingVenueCard;
-
-//vanue:{name,location,meta, media,price,rating},
+export default UserVenueCard;
