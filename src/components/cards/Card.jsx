7dbe2b20b1670c0596e3d368c48caa6adbff4211
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-09-15 14:17:41 +0200

    refine card design

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,5 +1,12 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { FaWifi } from "react-icons/fa";
+import { FaUtensils } from "react-icons/fa";
+import { FaPaw } from "react-icons/fa";
+import { FaCar } from "react-icons/fa";
+
+import NoImage from "/src/assets/images/no-image.jpg";
+const imageNotAvailable = "image not available";
 
 const Card = ({ venue }) => {
   const { id, name, price, location, media, meta } = venue;
@@ -9,31 +16,61 @@ const Card = ({ venue }) => {
   // const qs = "?_owner=true&_bookings=true"
 
   return (
-    <div className="container mx-auto mt-8 border border-red-500">
-      <Link
-        to={`/Venues/${id}`}
-        className="max-w-sm rounded overflow-hidden shadow-md hover:bg-slate-100 hover:shadow-lg bg-red-400 transition duration-300 transform hover:-translate-y-1"
-      >
-        <img className="w-full h-64 object-cover" src={media[0]} alt="{name}" />
-        <div className="px-6 py-4 border border-b-gray-200">
+    <Link to={`/Venues/${id}`} className="container mx-auto mt-8 w-80 h-full">
+      <div className="max-w-sm rounded overflow-hidden shadow-md hover:bg-slate-100 hover:shadow-lg bg-grey-400 transition duration-300 transform hover:-translate-y-1">
+        {media.length > 0 ? (
+          <img className="w-full h-64 object-cover" src={media[0]} alt={name} />
+        ) : (
+          <img
+            className="w-full h-64 object-cover"
+            src={NoImage}
+            alt={imageNotAvailable}
+          />
+        )}
+
+        <div className="px-6 py-4">
           <h5 className="font-bold text-xl mb-2">{name.toUpperCase()}</h5>
 
-          <p className="text-gray-700">
-            Location: {location.address}, {location.city}, {location.country}
+          <p className="text-gray-700 mb-4 border-b-2 border-gray-200">
+            {location.address}, {location.city}, {location.country}
           </p>
-          <ul className="text-gray-700 mt-2">
-            {Object.entries(meta).map(([key, value]) => (
-              <li key={key} className="flex items-center">
-                {value ? `${key}: Yes` : `${key}: No`}
-              </li>
-            ))}
-          </ul>
+          <div className="flex gap-6 my-6 ">
+            {meta.breakfast ? (
+              <div>
+                <FaUtensils />
+                <p>Breakfast</p>
+              </div>
+            ) : null}
+
+            {meta.wifi ? (
+              <div>
+                {" "}
+                <FaWifi />
+                <p>Wi fi</p>
+              </div>
+            ) : null}
+
+            {meta.pets ? (
+              <div>
+                {" "}
+                <FaPaw />
+                <p>Pets</p>{" "}
+              </div>
+            ) : null}
+
+            {meta.parking ? (
+              <div>
+                <FaCar />
+                <p>Parking</p>
+              </div>
+            ) : null}
+          </div>
         </div>
         <div className="px-6 pt-4 pb-2 flex items-center justify-between">
           <div className="text-xl font-semibold">${price} / night</div>
         </div>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 };
 
