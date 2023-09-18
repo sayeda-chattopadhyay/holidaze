49433055a852d0ce6e45fb1df42a668965eff4f4
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-09-18 22:52:44 +0200

    fixes all bug

--- src/pages/profile/createVenue/CreateVenueForm.jsx
+++ src/pages/profile/createVenue/CreateVenueForm.jsx
@@ -2,6 +2,7 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 import { useState } from "react";
 
+
 const validationSchema = Yup.object().shape({
   name: Yup.string()
     .min(6, "Name must be at least 6 characters")
@@ -41,77 +42,83 @@ const validationSchema = Yup.object().shape({
     .required("Continent is required"),
   media: Yup.string().url("Must be a valid URL").required("Required"),
 
-  // .required("Must have at least 1 image"),
-  // meta: Yup.object().shape({
-  //   wifi: Yup.boolean(),
-  //   parking: Yup.boolean(),
-  //   breakfast: Yup.boolean(),
-  //   pets: Yup.boolean(),
-  // }),
+  meta: Yup.object().shape({
+    wifi: Yup.boolean(),
+    parking: Yup.boolean(),
+    breakfast: Yup.boolean(),
+    pets: Yup.boolean(),
+  }),
 });
 
 const CreateVenueForm = () => {
   const [mediaArray, setMediaArray] = useState([]);
   console.log("mediaArray", mediaArray);
 
-  const { values, handleBlur, handleChange, handleSubmit, touched, errors } =
-    useFormik({
-      initialValues: {
-        name: "",
-        description: "",
-        media: [],
-        price: 0,
-        maxGuests: 0,
+  const {
+    values,
+    handleBlur,
+    handleChange,
+    handleSubmit,
+    touched,
+    errors,
+    setFieldValue,
+  } = useFormik({
+    initialValues: {
+      name: "",
+      description: "",
+      media: [],
+      price: 0,
+      maxGuests: 1,
+      location: {
+        address: "",
+        city: "",
+        zip: "",
+        country: "",
+        continent: "",
+      },
+      meta: {
+        wifi: false,
+        parking: false,
+        breakfast: false,
+        pets: false,
+      },
+    },
+
+    onSubmit: (values, action) => {
+      console.log("Form submitted with values:", values);
+
+      const formData = {
+        name: values.name,
+        description: values.description,
+        price: values.price,
+        maxGuests: values.maxGuests,
+        rating: 0, // Default rating
+        media: mediaArray,
         location: {
-          address: "",
-          city: "",
-          zip: "",
-          country: "",
-          continent: "",
+          address: values.address,
+          city: values.city,
+          zip: values.zip,
+          country: values.country,
+          continent: values.continent,
+          lat: 0, // Default latitude
+          lng: 0, // Default longitude
         },
-        // meta: {
-        //   wifi: false,
-        //   parking: false,
-        //   breakfast: false,
-        //   pets: false,
-        // },
-      },
+        meta: {
+          wifi: values.meta.wifi,
+          parking: values.meta.parking,
+          breakfast: values.meta.breakfast,
+          pets: values.meta.pets,
+        },
+      };
+      // api call
+      action.resetForm();
+      console.log("formData", formData);
+    },
+    validationSchema,
+  });
 
-      onSubmit: (values, action) => {
-        console.log("Form submitted with values:", values);
-
-        const formData = {
-          name: values.name,
-          description: values.description,
-          price: values.price,
-          maxGuests: values.maxGuests,
-          media: mediaArray,
-          location: {
-            address: values.address,
-            city: values.city,
-            zip: values.zip,
-            country: values.country,
-            continent: values.continent,
-            lat: 0, // Default latitude
-            lng: 0, // Default longitude
-          },
-
-          // meta: {
-          //   wifi: values.meta.wifi,
-          //   parking: values.meta.parking,
-          //   breakfast: values.meta.breakfast,
-          //   pets: values.meta.pets,
-          // },
-        };
-        // api call
-        action.resetForm();
-        console.log("venueFormvalue", formData);
-      },
-      validationSchema,
-    });
-
-  console.log("Formik error : ", errors);
-  console.log("Formik touched: ", touched);
+  // console.log("Formik error : ", errors);
+  // console.log("Formik touched: ", touched);
   console.log("Formik values: ", values);
 
   const removeMedia = (index) => {
@@ -133,7 +140,7 @@ const CreateVenueForm = () => {
   }
 
   return (
-    <>
+    <div className="container px-4 py-4 border border-red-600">
       <h1>Create Venue</h1>
       <form className="space-y-6" onSubmit={handleSubmit}>
         <div>
@@ -359,69 +366,13 @@ const CreateVenueForm = () => {
             ) : null}
           </div>
         </div>
-        {/* media preview section */}
-        {/* <div>
-          <label
-            htmlFor="media"
-            className="block text-lg font-medium leading-6 text-gray-900"
-          >
-            Upload venue images
-          </label>
-          {mediaArray && (
-            <div className="mt-4 flex flex-wrap gap-4">
-              {mediaArray.map((media, index) => (
-                <div key={index} className="relative h-24 w-24 rounded-md">
-                  <img
-                    src={media}
-                    alt="Images of the Accommodation"
-                    className="block h-full w-full rounded-md leading-6"
-                  />
-                  <button
-                    type="button"
-                    onClick={() => removeMedia(index)}
-                    className="absolute bottom-0 right-0 z-10 rounded-md bg-red-700 p-1 text-xs text-white"
-                  >
-                    Remove
-                  </button>
-                </div>
-              ))}
-            </div>
-          )}
-        </div> */}
-        {/* media upload section */}
-        {/* <div className="mt-2 w-full">
-          <input
-            type="text"
-            name="media"
-            id="media"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            // value={values.media}
-            placeholder="Add valid Image url here"
-            className="block w-full rounded-md border-0 px-2 py-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
-          />
-          <div className="flex justify-end">
-            <button
-              type="button"
-              onClick={pushToMediaArray}
-              className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-blue-600"
-            >
-              Add
-            </button>
-          </div>
-        </div>
 
-        {touched.media && errors.media ? (
-          <div className="text-red-600">{errors.media}</div>
-        ) : null} */}
-
-        {/* lisa start */}
         <div className="my-8 flex flex-col items-start">
           <label
             htmlFor="Images"
             className="block font-paragraph text-sm font-medium leading-6 text-gray-900"
           >
-            Images
+            Add Images
           </label>
           {mediaArray && (
             <div className="mt-4 flex flex-wrap gap-4">
@@ -468,17 +419,110 @@ const CreateVenueForm = () => {
             <div className="text-red-600">{errors.media}</div>
           ) : null}
         </div>
-        {/* lisa end */}
         <div>
+          {/* meta data start */}
+          <div className="mt-6">
+            <h3 className="mb-4 font-semibold text-gray-900">Add facilities</h3>
+            <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+              <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                <div className="flex items-center pl-3">
+                  <input
+                    id="wifi"
+                    name="meta.wifi"
+                    type="checkbox"
+                    onBlur={handleBlur}
+                    checked={values.meta.wifi}
+                    onChange={() => {
+                      setFieldValue("meta.wifi", !values.meta.wifi);
+                    }}
+                    // checked={values.wifi}
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                  />
+                  <label
+                    htmlFor="wifi"
+                    className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >
+                    Wi-fi
+                  </label>
+                </div>
+              </li>
+              <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                <div className="flex items-center pl-3">
+                  <input
+                    id="parking"
+                    name="meta.parking"
+                    type="checkbox"
+                    onBlur={handleBlur}
+                    checked={values.meta.parking}
+                    onChange={() => {
+                      setFieldValue("meta.parking", !values.meta.parking);
+                    }}
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                  />
+                  <label
+                    htmlFor="react-checkbox-list"
+                    className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >
+                    Parking
+                  </label>
+                </div>
+              </li>
+              <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                <div className="flex items-center pl-3">
+                  <input
+                    id="breakfast"
+                    name="meta.breakfast"
+                    type="checkbox"
+                    onBlur={handleBlur}
+                    checked={values.meta.breakfast}
+                    onChange={() => {
+                      setFieldValue("meta.breakfast", !values.meta.breakfast);
+                    }}
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                  />
+                  <label
+                    htmlFor="angular-checkbox-list"
+                    className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >
+                    Breakfast
+                  </label>
+                </div>
+              </li>
+              <li className="w-full dark:border-gray-600">
+                <div className="flex items-center pl-3">
+                  <input
+                    id="pets"
+                    name="meta.pets"
+                    type="checkbox"
+                    onBlur={handleBlur}
+                    checked={values.meta.pets}
+                    onChange={() => {
+                      setFieldValue("meta.pets", !values.meta.pets);
+                    }}
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                  />
+                  <label
+                    htmlFor="laravel-checkbox-list"
+                    className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  >
+                    Pets
+                  </label>
+                </div>
+              </li>
+            </ul>
+          </div>
+
+          {/* meta data end */}
+
           <button
             type="submit"
-            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className=" mt-4 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Create Venue
           </button>
         </div>
       </form>
-    </>
+    </div>
   );
 };
 
