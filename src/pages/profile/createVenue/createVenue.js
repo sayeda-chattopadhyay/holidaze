5696cfd5725722794934ba20cfcd53b2ea6843f9
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-09-19 15:07:32 +0200

    api function for creating venue

--- src/pages/profile/createVenue/createVenue.js
+++ src/pages/profile/createVenue/createVenue.js
@@ -1,35 +1,38 @@
 import { VENUES_URL } from "../../../constants";
-//import { authFetch } from "../../../helper/authorization";
+import { authFetch } from "../../../helper/authorization";
 
 export async function createVenue(formData) {
-  const accessToken = localStorage.getItem("token");
+  console.log("formData", formData);
 
+  const accessToken = localStorage.getItem("token");
   console.log("accessToken", accessToken);
 
-  try {
-    const url = VENUES_URL;
-    console.log(url);
+  const url = `${VENUES_URL}`;
+  console.log(url);
 
-    const response = await fetch(url, {
+  try {
+    const response = await authFetch(url, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${accessToken}`,
-      },
+
       body: JSON.stringify(formData),
     });
 
     console.log("response", response);
 
     if (!response.ok) {
-      throw new Error("Failed to create venue", response.status);
+      const errorMessage = `Failed to create venue. Status: ${response.status}`;
+      throw new Error(errorMessage);
     }
 
     const data = await response.json();
+
     console.log("venueFormdata", data);
+
     return data;
   } catch (error) {
     console.log("Create venue error", error.message);
     throw error;
   }
 }
+
+
