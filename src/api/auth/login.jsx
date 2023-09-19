2495d316558874c82dc4878d336e303bbbc760f3
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-09-19 19:04:15 +0200

    cleans code

--- src/api/auth/login.jsx
+++ src/api/auth/login.jsx
@@ -25,7 +25,7 @@ export function useLogin() {
       if (response.ok) {
         const { accessToken, ...profileDetails } = await response.json();
 
-        // Save the token and profile using your auth functions
+        // Save the token and profile using  auth functions
         auth.save("token", accessToken);
         auth.save("profile", profileDetails);
 
@@ -42,44 +42,4 @@ export function useLogin() {
   return { logIn };
 }
 
-// end
 
-// import { LOGIN_URL } from "../../constants";
-
-// import * as storage from "../../storage/index.mjs";
-
-// const loginURL = LOGIN_URL;
-
-// const method = "post";
-
-// export async function logIn(profile) {
-//   try {
-//     const url = loginURL;
-
-//     const userToLoginObject = {
-//       headers: {
-//         "content-Type": "application/json",
-//       },
-//       method,
-//       body: JSON.stringify(profile),
-//     };
-
-//     const response = await fetch(url, userToLoginObject);
-//     console.log(response);
-
-//     if (response.ok) {
-//       const { accessToken, ...profileDetails } = await response.json(); //  destructuring(for storing profile and access token separately)
-
-//       storage.save("token", accessToken);
-
-//       storage.save("profile", profileDetails);
-//     } else {
-//       throw new Error("Incorrect username or password");
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     throw error;
-//   }
-// }
-
-//import { useAuth } from "../../context/authUtils.js";
