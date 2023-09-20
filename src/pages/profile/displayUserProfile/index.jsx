068cebb842729e24832c30005b711a569bb37ccf
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-09-20 12:32:32 +0200

    adds placeholder image for no avatar

--- src/pages/profile/displayUserProfile/index.jsx
+++ src/pages/profile/displayUserProfile/index.jsx
@@ -4,11 +4,15 @@ import { useState } from "react";
 import BookingVenueCard from "../userBookings";
 import UserVenueCard from "../usersVenues";
 import CreateVenueForm from "../createVenue/CreateVenueForm";
+import noAvatarImage from "/src/assets/images/no-avatar.png";
 
 const DisplayProfile = ({ profile }) => {
   const { name, avatar, email, venueManager, bookings, venues } = profile;
   const [activeTab, setActiveTab] = useState("bookings");
 
+  const newAvatar = avatar ? avatar : noAvatarImage;
+
+  console.log("Profile avatar:", avatar);
   console.log("profile Info:", profile);
 
   return (
@@ -16,7 +20,7 @@ const DisplayProfile = ({ profile }) => {
       <div className="container mx-auto px-10 py-10 max-w-2xl bg-white rounded-lg shadow-lg">
         <div className="flex flex-col items-center md:flex-row md:justify-center gap-10 md:space-x-4">
           <img
-            src={avatar}
+            src={newAvatar}
             alt={name}
             className="rounded-full w-32 h-32 md:w-50 md:h-50"
           />
@@ -30,91 +34,64 @@ const DisplayProfile = ({ profile }) => {
           </div>
         </div>
       </div>
-
+      {/* tab start */}
       <div className="container mx-auto mt-10">
         {venueManager ? (
-           <div className="flex-col md:flex md:flex-row justify-between items-center max-w-sm mx-auto md:max-w-lg px-4 py-4 ">
-          
-           <div
-             className={`border border-blue-500 px-4 py-4 cursor-pointer ${
-               activeTab === "bookings"
-                 ? "bg-red-500 text-white"
-                 : "bg-white text-red-500 hover:bg-red-100 hover:text-red-700"
-             }`}
-             onClick={() => setActiveTab("bookings")}
-           >
-             <h3 className="text-xl font-customFont text-center">
-               Your Upcoming Bookings
-             </h3>
-           </div>
-           <div
-             className={`border border-red-500 px-4 py-4 cursor-pointer ${
-               activeTab === "venues"
-                 ? "bg-red-500 text-white"
-                 : "bg-white text-red-500 hover:bg-red-100 hover:text-red-700"
-             }`}
-             onClick={() => setActiveTab("venues")}
-           >
-             <h1 className="text-3xl font-customFont text-center">
-               Your Rented Venues
-             </h1>
-           </div>
-           <div
-             className={`border border-red-500 px-4 py-4 cursor-pointer ${
-               activeTab === "expiredBookings"
-                 ? "bg-red-500 text-white"
-                 : "bg-white text-red-500 hover:bg-red-100 hover:text-red-700"
-             }`}
-             onClick={() => setActiveTab("createVenue")}
-           >
-             <h1 className="text-3xl font-customFont text-center">
-               Create New Venue
-             </h1>
-           </div>
-         </div>
-
-        ): (
           <div className="flex-col md:flex md:flex-row justify-between items-center max-w-sm mx-auto md:max-w-lg px-4 py-4 ">
-          
-          <div
-            className={`border border-blue-500 px-4 py-4 cursor-pointer ${
-              activeTab === "bookings"
-                ? "bg-red-500 text-white"
-                : "bg-white text-red-500 hover:bg-red-100 hover:text-red-700"
-            }`}
-            onClick={() => setActiveTab("bookings")}
-          >
-            <h3 className="text-xl font-customFont text-center">
-              Your Upcoming Bookings
-            </h3>
+            <div
+              className={`border border-blue-500 px-4 py-4 cursor-pointer ${
+                activeTab === "bookings"
+                  ? "bg-red-500 text-white"
+                  : "bg-white text-red-500 hover:bg-red-100 hover:text-red-700"
+              }`}
+              onClick={() => setActiveTab("bookings")}
+            >
+              <h3 className="text-xl font-customFont text-center">
+                Your Upcoming Bookings
+              </h3>
+            </div>
+            <div
+              className={`border border-red-500 px-4 py-4 cursor-pointer ${
+                activeTab === "venues"
+                  ? "bg-red-500 text-white"
+                  : "bg-white text-red-500 hover:bg-red-100 hover:text-red-700"
+              }`}
+              onClick={() => setActiveTab("venues")}
+            >
+              <h1 className="text-3xl font-customFont text-center">
+                Your Rented Venues
+              </h1>
+            </div>
+            <div
+              className={`border border-red-500 px-4 py-4 cursor-pointer ${
+                activeTab === "expiredBookings"
+                  ? "bg-red-500 text-white"
+                  : "bg-white text-red-500 hover:bg-red-100 hover:text-red-700"
+              }`}
+              onClick={() => setActiveTab("createVenue")}
+            >
+              <h1 className="text-3xl font-customFont text-center">
+                Create New Venue
+              </h1>
+            </div>
           </div>
-          {/* <div
-            className={`border border-red-500 px-4 py-4 cursor-pointer ${
-              activeTab === "venues"
-                ? "bg-red-500 text-white"
-                : "bg-white text-red-500 hover:bg-red-100 hover:text-red-700"
-            }`}
-            onClick={() => setActiveTab("venues")}
-          >
-            <h1 className="text-3xl font-customFont text-center">
-              Your Rented Venues
-            </h1>
+        ) : (
+          <div className="flex-col md:flex md:flex-row justify-between items-center max-w-sm mx-auto md:max-w-lg px-4 py-4 ">
+            <div
+              className={`border border-blue-500 px-4 py-4 cursor-pointer ${
+                activeTab === "bookings"
+                  ? "bg-red-500 text-white"
+                  : "bg-white text-red-500 hover:bg-red-100 hover:text-red-700"
+              }`}
+              onClick={() => setActiveTab("bookings")}
+            >
+              <h3 className="text-xl font-customFont text-center">
+                Your Upcoming Bookings
+              </h3>
+            </div>
           </div>
-          <div
-            className={`border border-red-500 px-4 py-4 cursor-pointer ${
-              activeTab === "expiredBookings"
-                ? "bg-red-500 text-white"
-                : "bg-white text-red-500 hover:bg-red-100 hover:text-red-700"
-            }`}
-            onClick={() => setActiveTab("createVenue")}
-          >
-            <h1 className="text-3xl font-customFont text-center">
-              Create New Venue
-            </h1>
-          </div> */}
-        </div>
         )}
-        
+        {/* tab end */}
         {/* render booking, rented venue, create venue form */}
         {activeTab === "bookings" && (
           <div className="container mx-auto px-10 py-10 max-w-2xl bg-gray-100 rounded-lg shadow-lg mt-10">
@@ -133,14 +110,14 @@ const DisplayProfile = ({ profile }) => {
         {activeTab === "venues" && (
           <div className="container mx-auto px-4 py-4  bg-gray-100 rounded-lg shadow-lg mt-10">
             <h1 className="text-center text-xl md:text-2xl bg-gray-200 py-2 rounded-lg">
-              Your Venues
+              All venues created by {name}
             </h1>
             {venues && venues.length > 0 ? (
               venues.map((userVenue) => (
                 <UserVenueCard key={userVenue.id} userVenue={userVenue} />
               ))
             ) : (
-              <p>No venues yet</p>
+              <p>No Venues created by you</p>
             )}
           </div>
         )}
@@ -174,8 +151,6 @@ DisplayProfile.propTypes = {
         }),
       })
     ),
-    
-    
   }).isRequired,
 };
 
