6d0ea09002b70005695c9c7a92a3ed8ef97a5080
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-09-25 12:05:03 +0200

    creates errorMessasge component

--- src/components/ui/ErrorMessage.jsx
+++ src/components/ui/ErrorMessage.jsx
@@ -1,8 +1,7 @@
 
-
-const ErrorMessage = () => {
+const ErrorMessage = ({ message }) => {
   return (
-    <div>ErrorMessage</div>
+    <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-2 rounden-md mt-4">{message}</div>
   )
 }
 
