d8f3eb30ca2418692e4d460d21dddea15a755d73
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-09-26 19:05:20 +0200

    rectify signUp folder name

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Route, Routes } from "react-router-dom";
 // pages
 
 import HomePage from "./pages/home";
-import SignUpPage from "./pages/signup";
+import SignUpPage from "./pages/signUp";
 import LoginPage from "./pages/logIn";
 import VenuesPage from "./pages/venues";
 import SpecificVenue from "./pages/specificVenue";
@@ -35,7 +35,7 @@ function App() {
         <Route path="/" element={<BaseLayout />}>
           <Route index element={<HomePage />} />
           <Route path="/logIn" element={<LoginPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/signUp" element={<SignUpPage />} />
           <Route path="/venues" element={<VenuesPage />} />
           <Route path="/venues/:id" element={<SpecificVenue />} />
           <Route path="/profile" element={<ProfilePage />} />
