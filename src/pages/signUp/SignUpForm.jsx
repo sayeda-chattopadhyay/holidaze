3228442e91ae820cb4eb7c6364d9223f97ac0124
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-09-28 17:36:35 +0200

    changes style

--- src/pages/signUp/SignUpForm.jsx
+++ src/pages/signUp/SignUpForm.jsx
@@ -78,17 +78,10 @@ const SignUpForm = () => {
 
   return (
     <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign Up
-          </h2>
-        </div>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-6 lg:px-8 bg-white shadow-md rounded-lg overflow-hidden mx-auto max-w-xl mt-10">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign Up
+        </h2>
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form
             className="space-y-6"
