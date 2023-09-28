222c797fbfb3fde94f408634895d411edfaaae84
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-09-28 17:27:24 +0200

    change style

--- src/pages/signUp/SignUpText.jsx
+++ src/pages/signUp/SignUpText.jsx
@@ -1,15 +1,24 @@
 import { NavLink } from "react-router-dom";
+import CardImage from "/src/assets/images/card-image.jpg";
 
 const TextSignUp = () => {
   return (
-    <>
-      <h1>Holidaze</h1>
-      <img src="/src/assets/images/card-image.jpg" alt="image of a venue" />
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis, ex.
-      </p>
-      <NavLink to="/venues">Explore venues</NavLink>
-    </>
+    <div className="bg-white shadow-md rounded-lg overflow-hidden mx-auto max-w-xl">
+      <img src={CardImage} alt="Image of a venue" className="w-full h-auto" />
+      <div className="p-4">
+        <h1 className="text-2xl font-semibold mb-2">Holidaze</h1>
+        <p className="text-gray-600">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis,
+          ex.
+        </p>
+        <NavLink
+          to="/venues"
+          className="bg-blue-500 text-white px-4 py-2 mt-4 inline-block hover:bg-blue-600 rounded-lg"
+        >
+          Explore venues
+        </NavLink>
+      </div>
+    </div>
   );
 };
 
