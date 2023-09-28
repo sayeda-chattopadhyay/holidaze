1382e427461034e46e3a987b370fe87f23ab90b5
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-09-28 11:19:02 +0200

    add limits to the venues url

--- src/pages/venues/AllVenues.jsx
+++ src/pages/venues/AllVenues.jsx
@@ -6,9 +6,11 @@ import SearchVenues from "../../components/search/SearchVenues";
 import LoadingIndicator from "../../components/ui/LoadingIndicator";
 import ErrorMessage from "../../components/ui/ErrorMessage";
 
-const qs = "?sort=created&sortOrder=desc&&_owner=true&_bookings=true";
+const venuesLimit = 90;
+const qs = `?sort=created&sortOrder=desc&&_owner=true&_bookings=true&limit=${venuesLimit}`;
 //const qs = "?sort=created&sortOrder=Desc&&_owner=true&_bookings=true"; // this code is to check error message
 const allVenuesUrl = VENUES_URL + qs;
+// const allVenuesUrl = VENUES_URL + "?sort=created&sortOrder=desc&&_owner=true&_bookings=true";
 
 const AllVenues = () => {
   const [search, setSearch] = useState("");
