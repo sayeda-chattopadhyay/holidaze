96db93fc92ee6b14d803b35c0d91675c110d0f22
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-09-29 20:39:33 +0200

    adds hover color

--- src/pages/signUp/SignUpText.jsx
+++ src/pages/signUp/SignUpText.jsx
@@ -13,7 +13,7 @@ const TextSignUp = () => {
         </p>
         <NavLink
           to="/venues"
-          className="bg-secondary text-white px-4 py-2 mt-4 inline-block hover:bg--600 rounded-lg"
+          className="bg-secondary text-white px-4 py-2 mt-4 inline-block hover:bg-secondary-lighter rounded-lg"
         >
           Explore venues
         </NavLink>
