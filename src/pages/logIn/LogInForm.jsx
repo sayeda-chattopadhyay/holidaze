83b69f01446675fda0a503dd5e7ba0b6db27c406
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-09-30 01:00:23 +0200

    removes logo

--- src/pages/logIn/LogInForm.jsx
+++ src/pages/logIn/LogInForm.jsx
@@ -52,11 +52,6 @@ const LogInForm = () => {
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       {isLoading && <LoadingIndicator />}
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img
-          className="mx-auto h-10 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt="Your Company"
-        />
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           Log In
         </h2>
