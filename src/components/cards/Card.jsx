8d527f0fc19423e03371d081cad6531737872fad
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-09-29 19:21:59 +0200

    change proptype for id to string

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -10,16 +10,9 @@ const imageNotAvailable = "image not available";
 
 const Card = ({ venue }) => {
   const { id, name, price, location, media, meta } = venue;
-  console.log(venue);
-  // console.log(id);
-
-  // const qs = "?_owner=true&_bookings=true"
 
   return (
-    <Link
-      to={`/Venues/${id}`}
-      className="container mx-auto mt-8 w-80 h-full"
-    >
+    <Link to={`/Venues/${id}`} className="container mx-auto mt-8 w-80 h-full">
       <div className="max-w-sm rounded overflow-hidden shadow-md hover:bg-slate-100 hover:shadow-lg bg-grey-400 transition duration-300 transform hover:-translate-y-1 flex flex-col h-full">
         {media.length > 0 ? (
           <img className="w-full h-64 object-cover" src={media[0]} alt={name} />
@@ -79,7 +72,7 @@ const Card = ({ venue }) => {
 
 Card.propTypes = {
   venue: PropTypes.shape({
-    id: PropTypes.number.isRequired,
+    id: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
     description: PropTypes.string.isRequired,
