55ef829f733fe590d6aec97fe94803b36371f5e2
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-09-29 13:51:28 +0200

    chenges style

--- src/pages/signUp/SignUpText.jsx
+++ src/pages/signUp/SignUpText.jsx
@@ -6,14 +6,14 @@ const TextSignUp = () => {
     <div className="bg-white shadow-md rounded-lg overflow-hidden mx-auto max-w-xl">
       <img src={CardImage} alt="Image of a venue" className="w-full h-auto" />
       <div className="p-4">
-        <h1 className="text-2xl font-semibold mb-2">Holidaze</h1>
-        <p className="text-gray-600">
+        <h1 className="text-3xl font-heading mb-2">Holidaze</h1>
+        <p className="text-gray-600 font-paragraph">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis,
           ex.
         </p>
         <NavLink
           to="/venues"
-          className="bg-blue-500 text-white px-4 py-2 mt-4 inline-block hover:bg-blue-600 rounded-lg"
+          className="bg-secondary text-white px-4 py-2 mt-4 inline-block hover:bg--600 rounded-lg"
         >
           Explore venues
         </NavLink>
