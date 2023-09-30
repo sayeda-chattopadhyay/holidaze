fc014d2cbda774b88e22025d0832f03b2abe0486
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-09-30 17:30:33 +0200

    add  sort order to the iürl

--- src/hooks/useSingleProfile/index.jsx
+++ src/hooks/useSingleProfile/index.jsx
@@ -3,7 +3,8 @@ import { authFetch } from "../../helper/authorization";
 import { PROFILE_URL } from "../../constants";
 
 export function useSingleProfile(name) {
-  const url = `${PROFILE_URL}${name}?_bookings=true&_venues=true`;
+  // const url = `${PROFILE_URL}${name}?_bookings=true&_venues=true`;
+  const url = `${PROFILE_URL}${name}?_bookings=true&_venues=true&sortOrder=desc&sort=created`;
   const [singleProfile, setSingleProfile] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
@@ -14,9 +15,19 @@ export function useSingleProfile(name) {
       setIsError(false);
       const response = await authFetch(url);
       const json = await response.json();
-      console.log(json);
+      console.log("singleProfile", json);
 
       setSingleProfile(json);
+
+      // Sort bookings by date in descending order
+      //  const sortedBookings = json.bookings.sort((a, b) => {
+      //   const dateA = new Date(a.dateFrom).getTime();
+      //   const dateB = new Date(b.dateFrom).getTime();
+      //   return dateB - dateA;
+      // });
+      // setSingleProfile({ ...json, bookings: sortedBookings });
+
+
     } catch (error) {
       console.log(error);
       setIsError(true);
