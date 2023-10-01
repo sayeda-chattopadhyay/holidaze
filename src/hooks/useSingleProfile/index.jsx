2444fc573cc05389e40fbbd007edcc756dfd12ca
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-10-01 22:32:29 +0200

    cleans code

--- src/hooks/useSingleProfile/index.jsx
+++ src/hooks/useSingleProfile/index.jsx
@@ -3,7 +3,6 @@ import { authFetch } from "../../helper/authorization";
 import { PROFILE_URL } from "../../constants";
 
 export function useSingleProfile(name) {
-  // const url = `${PROFILE_URL}${name}?_bookings=true&_venues=true`;
   const url = `${PROFILE_URL}${name}?_bookings=true&_venues=true&sortOrder=desc&sort=created`;
   const [singleProfile, setSingleProfile] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
@@ -15,21 +14,9 @@ export function useSingleProfile(name) {
       setIsError(false);
       const response = await authFetch(url);
       const json = await response.json();
-      console.log("singleProfile", json);
 
       setSingleProfile(json);
-
-      // Sort bookings by date in descending order
-      //  const sortedBookings = json.bookings.sort((a, b) => {
-      //   const dateA = new Date(a.dateFrom).getTime();
-      //   const dateB = new Date(b.dateFrom).getTime();
-      //   return dateB - dateA;
-      // });
-      // setSingleProfile({ ...json, bookings: sortedBookings });
-
-
     } catch (error) {
-      console.log(error);
       setIsError(true);
     } finally {
       setIsLoading(false);
