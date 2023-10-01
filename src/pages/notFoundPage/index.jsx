f7b89e8dde08eb41eaa053fc297aebcd601156ee
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-10-01 16:49:06 +0200

    page not found message

--- src/pages/notFoundPage/index.jsx
+++ src/pages/notFoundPage/index.jsx
@@ -1,5 +1,17 @@
+import { NavLink } from "react-router-dom";
+
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <div className="container max-w-4xl mx-auto text-center px-10 py-10 my-40 border border-gray-200 ">
+      <h1 className="text-3xl p-6">Page not Found</h1>
+      <NavLink
+        to="/"
+        className="text-xl font-semibold text-primary hover:text-yellow-300 hover:underline"
+      >
+        Click here to go to Home Page
+      </NavLink>
+    </div>
+  );
 };
 
 export default ErrorPage;
