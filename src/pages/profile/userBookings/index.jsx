d0aad6bb99751b967cdaa8a81af4673cbf3e32f8
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-10-01 19:49:22 +0200

    remove carousel

--- src/pages/profile/userBookings/index.jsx
+++ src/pages/profile/userBookings/index.jsx
@@ -1,9 +1,3 @@
-//import { Link } from "react-router-dom";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { BiSolidRightArrow } from "react-icons/bi";
-import { BiSolidLeftArrow } from "react-icons/bi";
-
 import NoImage from "/src/assets/images/no-image.jpg";
 const imageNotAvailable = "image not available";
 
@@ -16,8 +10,6 @@ const BookingVenueCard = ({ booking }) => {
     venue: { name, location, meta, media, price },
   } = booking;
 
-  console.log("booking:", booking);
-
   function formatDate(dateString) {
     const date = new Date(dateString);
     const day = date.getDate().toString().padStart(2, "0");
@@ -26,85 +18,19 @@ const BookingVenueCard = ({ booking }) => {
     return `${day}/${month}/${year}`;
   }
 
+  const imageSrc = media && media.length > 0 ? media[0] : NoImage;
+  const altText = media && media.length > 0 ? name : imageNotAvailable;
+
   return (
     <div className="container px-4 py-4 ">
       <div className="max-w-xl mx-auto bg-white px-2 py-2 rounded-lg overflow-hidden shadow-md md:flex md:flex-row md:gap-10 justify-between items-center">
-        {media && media.length > 1 ? (
-          <Carousel
-            showThumbs={false}
-            showStatus={false}
-            showArrows={true}
-            renderArrowPrev={(onClickHandler, hasPrev, label) =>
-              hasPrev && (
-                <button
-                  type="button"
-                  onClick={onClickHandler}
-                  title={label}
-                  style={{
-                    position: "absolute",
-                    left: "10px",
-                    top: "50%",
-                    zIndex: "2",
-                    background: "transparent",
-                    border: "none",
-                    cursor: "pointer",
-                    color: "blue",
-                  }}
-                >
-                  <BiSolidLeftArrow />
-                </button>
-              )
-            }
-            renderArrowNext={(onClickHandler, hasNext, label) =>
-              hasNext && (
-                <button
-                  type="button"
-                  onClick={onClickHandler}
-                  title={label}
-                  style={{
-                    position: "absolute",
-                    right: "10px",
-                    top: "50%",
-                    zIndex: "2",
-                    background: "transparent",
-                    border: "none",
-                    cursor: "pointer",
-                    color: "blue",
-                    size: "24px",
-                  }}
-                >
-                  <BiSolidRightArrow />
-                </button>
-              )
-            }
-          >
-            {media.map((mediaItem, index) => (
-              <div key={index}>
-                <img
-                  className="w-full h-52 object-cover rounded-lg shadow-sm"
-                  src={mediaItem}
-                  alt={`${name} Image ${index + 1}`}
-                />
-              </div>
-            ))}
-          </Carousel>
-        ) : media && media.length === 1 ? (
+        <div className="md:w-1/2">
           <img
-            className="w-full h-48 object-cover md:w-1/2 rounded-lg shadow-sm"
-            src={media[0]}
-            alt={name}
+            className="w-full h-48 object-cover rounded-lg shadow-sm mb-4 md:mb-0"
+            src={imageSrc}
+            alt={altText}
           />
-        ) : (
-          <img
-            className="w-full h-48 object-cover md:w-1/2 rounded-lg shadow-sm"
-            src={NoImage}
-            alt={imageNotAvailable}
-          />
-        )}
-
-
-
-        {/* media end */}
+        </div>
         <div className="p-4 md:w-1/2">
           <h2 className="text-2xl font-semibold mb-2">{name}</h2>
           <p className="text-sm text-gray-600 mb-2">
@@ -117,15 +43,6 @@ const BookingVenueCard = ({ booking }) => {
             Check-out: {formatDate(dateTo)}
           </p>
           <p className="text-sm text-gray-600 mb-2">Guests: {guests}</p>
-          {/* will add delete booking */}
-          {/* <div className="flex justify-between mt-4">
-            <Link
-              to={`/venueBookedByUser/${id}`}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-            >
-              View More
-            </Link>
-          </div> */}
         </div>
       </div>
     </div>
@@ -134,4 +51,4 @@ const BookingVenueCard = ({ booking }) => {
 
 export default BookingVenueCard;
 
-//vanue:{name,location,meta, media,price,rating},
+
