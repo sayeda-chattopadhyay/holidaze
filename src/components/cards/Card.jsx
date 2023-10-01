d9cc8754ffa06be5644e5ddc115f91b30ff8066f
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-10-01 22:36:01 +0200

    cleans code

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,14 +1,7 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-// import { FaWifi } from "react-icons/fa";
-// import { FaUtensils } from "react-icons/fa";
-// import { FaPaw } from "react-icons/fa";
-// import { FaCar } from "react-icons/fa";
 import MediaComponent from "/src/components/ui/MediaComponent.jsx";
 import MetaIcons from "/src/components/ui/MetaIcons.jsx";
-
-// import NoImage from "/src/assets/images/no-image.jpg";
-// const imageNotAvailable = "image not available";
 
 const Card = ({ venue }) => {
   const { id, name, price, location, media, meta } = venue;
@@ -16,16 +9,6 @@ const Card = ({ venue }) => {
   return (
     <Link to={`/Venues/${id}`} className="container mx-auto mt-8 w-80 h-full">
       <div className="max-w-sm rounded overflow-hidden shadow-md hover:bg-slate-100 hover:shadow-lg bg-grey-400 transition duration-300 transform hover:-translate-y-1 flex flex-col h-full">
-        {/* {media.length > 0 ? (
-          <img className="w-full h-64 object-cover" src={media[0]} alt={name} />
-        ) : (
-          <img
-            className="w-full h-64 object-cover"
-            src={NoImage}
-            alt={imageNotAvailable}
-          />
-        )} */}
-
         <MediaComponent media={media} name={name} />
 
         <div className="px-6 py-4 flex-grow">
@@ -34,37 +17,8 @@ const Card = ({ venue }) => {
           <p className="text-gray-700 mb-4 border-b-2 border-gray-200">
             {location.address}, {location.city}, {location.country}
           </p>
-          <div className="mt-10" >
+          <div className="mt-10">
             <MetaIcons meta={meta} />
-            {/* {meta.breakfast ? (
-              <div>
-                <FaUtensils />
-                <p>Breakfast</p>
-              </div>
-            ) : null}
-
-            {meta.wifi ? (
-              <div>
-                {" "}
-                <FaWifi />
-                <p>Wi fi</p>
-              </div>
-            ) : null}
-
-            {meta.pets ? (
-              <div>
-                {" "}
-                <FaPaw />
-                <p>Pets</p>{" "}
-              </div>
-            ) : null}
-
-            {meta.parking ? (
-              <div>
-                <FaCar />
-                <p>Parking</p>
-              </div>
-            ) : null} */}
           </div>
         </div>
         <div className="px-6 pt-4 pb-2 flex items-center justify-end">
