bad7540d195a8f8b4fe3c7f915759c78c478f2b3
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-10-01 21:25:16 +0200

    adds primary color to the button

--- src/pages/home/banner/index.jsx
+++ src/pages/home/banner/index.jsx
@@ -18,7 +18,7 @@ const HeroBanner = () => {
         <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
           <NavLink
             to="/venues"
-            className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+            className="inline-flex justify-center items-center py-3 px-5 text-base font-medium font-heading text-center text-white rounded-lg bg-primary hover:bg-primary-darker focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
           >
             Explore Venues
           </NavLink>
