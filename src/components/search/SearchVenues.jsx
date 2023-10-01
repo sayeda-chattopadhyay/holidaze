f10c01e93d17f942e3fcc5b312c6997d99925f9e
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-10-01 23:11:04 +0200

    adds heading and changes color

--- src/components/search/SearchVenues.jsx
+++ src/components/search/SearchVenues.jsx
@@ -20,12 +20,15 @@ const SearchVenues = ({ onSearch }) => {
   return (
     <div className="container max-w-4xl mx-auto mt-20">
       <div className="max-w-md rounded overflow-hidden mx-auto p-1 font-pins text-fontcolor">
-        <div className="w-full max-w-md pb-3 pt-3 px-6">
-          <div className="flex items-center border-2 border-gray-300 bg-quaternary rounded-full py-2 px-2">
+        <div className="flex flex-col items-center sm:flex-row sm:items-center w-full max-w-lg pb-3 pt-3 px-6 bg-quaternary last:rounded-lg text-primary-darker mb-10">
+          <h1 className="text-2xl font-bold text-center sm:text-left sm:mr-4 mb-4 sm:mb-0">
+            Find Your Best Destination
+          </h1>
+          <div className="flex items-center border-2 border-gray-300 bg-white rounded-full py-2 px-2">
             <input
-              className="w-full pl-4 pr-10 py-2 leading-tight focus:outline-none bg-quaternary-lighter "
+              className="w-full pl-4 pr-10 py-2 leading-tight focus:outline-none bg-white"
               type="text"
-              placeholder="Search for a venue"
+              placeholder="Search.."
               value={search}
               onChange={handleSearch}
             />
@@ -35,7 +38,7 @@ const SearchVenues = ({ onSearch }) => {
                 onClick={handleClearSearch}
               />
             ) : (
-              <BsSearch className="w-6 h-6 text-gray-500" />
+              <BsSearch className="w-6 h-6 text-primary-lighter" />
             )}
           </div>
         </div>
@@ -50,5 +53,3 @@ SearchVenues.propTypes = {
 };
 
 export default SearchVenues;
-
-
