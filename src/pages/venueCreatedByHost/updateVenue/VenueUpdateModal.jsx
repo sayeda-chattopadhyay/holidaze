96eb7d2a14e26467b358f577fbbddae08c332d19
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-10-01 21:11:18 +0200

    applies custome button style to edit button

--- src/pages/venueCreatedByHost/updateVenue/VenueUpdateModal.jsx
+++ src/pages/venueCreatedByHost/updateVenue/VenueUpdateModal.jsx
@@ -4,23 +4,15 @@ import UpdateVenueForm from "./UpdateVenueForm";
 export default function VenueUpdateModal({ specificVenue }) {
   const [showModal, setShowModal] = useState(false);
 
+  // function to close the modal
 
-
-// function to close the modal
-
-const closeModal = () =>{
-  setShowModal(false);
-
-};
-
+  const closeModal = () => {
+    setShowModal(false);
+  };
 
   return (
     <>
-      <button
-        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-        type="button"
-        onClick={() => setShowModal(true)}
-      >
+      <button className="btnMedium" type="button" onClick={() => setShowModal(true)}>
         Edit Venue
       </button>
       {showModal && (
@@ -52,7 +44,10 @@ const closeModal = () =>{
               </div>
 
               {/* Update Venue Form */}
-              <UpdateVenueForm specificVenue={specificVenue}  closeModal= {closeModal}/>
+              <UpdateVenueForm
+                specificVenue={specificVenue}
+                closeModal={closeModal}
+              />
             </div>
           </div>
         </div>
